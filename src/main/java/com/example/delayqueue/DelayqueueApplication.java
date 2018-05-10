9b6d68578bad16fc6a4e7ee27e3190b90e3294c0
lvyangzhuo <lvyangzhuo@163.com>
2018-05-10 20:00:38 +0800

    简单的消息延迟队列使用

---
+++ src/main/java/com/example/delayqueue/DelayqueueApplication.java
@@ -0,0 +1,12 @@
+package com.example.delayqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelayqueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelayqueueApplication.class, args);
+    }
+}
